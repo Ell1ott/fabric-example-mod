e39cc1a16497a17b97dd7ce700d413836875c56d
Elliott <dbtracker.dk@gmail.com>
2022-05-03 15:53:22 +0200

    start

--- src/main/java/player/control/pControl.java
+++ src/main/java/player/control/pControl.java
@@ -4,7 +4,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+import net.minecraft.world.World;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.entity.BlockEntity;
+
+public class pControl implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
